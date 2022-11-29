707e3f7bafed42accf8173d5e10cbc2bcf44b1ce
Long vo <longvt@minerva.vn>
2022-11-29 22:55:24 +0700

    update project

---
+++ src/screens/global/SideBar.tsx
@@ -0,0 +1,11 @@
+import {Box, IconButton, useTheme} from "@mui/material";
+import {useContext} from "react";
+import {ColorModeContext, tokens} from "../../theme";
+import {InputBase} from "@mui/material";
+import {LightModeOutlined} from "@mui/icons-material"
+
+const SideBar = () => {
+  return <div> Sidebar </div>
+}
+
+export default SideBar
